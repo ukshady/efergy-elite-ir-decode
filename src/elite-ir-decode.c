9aad2c5ab15eb81993430454293a5b64cc85435a
Severn Tsui <severn@xephris.net>
2015-04-24 00:21:16 -0400

    Fix bit order (MSB) and add CRC16 verification

--- src/elite-ir-decode.c
+++ src/elite-ir-decode.c
@@ -2,6 +2,13 @@
 #include<stdint.h>
 #include<string.h>
 
+enum Mode
+{
+    None = 0,
+    NegPreamble = 1,
+    Decode = 2
+};
+
 // Data about a message from the Elite IR
 struct msgdata
 {
@@ -16,9 +23,51 @@ struct msgdata
     uint8_t bit;    // current bit to write
 
     // 0 = preamble not found
-    // 1 = preamble found, start decoding
+    // 1 = found 100+ -ve
+    // 2 = preamble found, start decoding
     uint8_t mode;
 };
+
+uint16_t crc_table[256];
+
+void init_crc_table()
+{
+    const uint16_t poly = 0x1021U;
+
+    for(int i = 0; i < 256; ++i)
+    {
+        uint16_t v = 0;
+        uint16_t t = i << 8;
+        
+        for(int j = 0; j < 8; ++j)
+        {
+            if(((v ^ t) & 0x8000U) )
+            {
+                v = ((v << 1) ^ poly);
+            }
+            else
+            {
+                v <<= 1;
+            }
+            
+            t <<= 1;
+        }
+        
+        crc_table[i] = v;
+    }
+}
+
+uint16_t crc16xmodem ( uint8_t * d, int len )
+{
+    uint16_t crc = 0;
+    
+    for ( int i = 0; i < len; ++i )
+    {
+        crc = (uint16_t)( ( crc << 8 ) ^ crc_table[( ( crc >> 8 ) ^ d[i] ) & 0xff] );
+    }
+    
+    return crc;
+}
 
 int get_sample ( FILE * f, int16_t * val )
 {
@@ -30,21 +79,6 @@ int get_sample ( FILE * f, int16_t * val )
     *val = ( v1 & 0xff ) | ( ( v2 & 0xff ) << 8 );
 
     return 1;
-}
-
-int calc_checksum ( uint8_t * buf, uint8_t len )
-{
-    if ( len < 10 )
-        return -1;
-
-    int csum = 0;
-
-    for ( int i = 0; i < 10; ++i )
-    {
-        csum += buf[i];
-    }
-
-    return ( csum % 256 );
 }
 
 int main( int argc, char *argv[] )
@@ -66,6 +100,14 @@ int main( int argc, char *argv[] )
         f = fopen ( argv[1], "r" );
     }
 
+    if ( !f )
+    {
+        fprintf ( stderr, "Failed to open input\n" );
+        return -1;
+    }
+
+    init_crc_table();
+
     int16_t s = 0;
 
     struct msgdata d;
@@ -76,63 +118,34 @@ int main( int argc, char *argv[] )
         if ( s > 0 )
         {
             ++d.n_up;
-
-            // 40+ samples down while in mode 1 = end of message
-            if ( d.mode == 1 && d.n_down > 40 )
-            {
-                d.mode = 0;
-
-                printf ( "\nbyte: %d, bit: %d\n", d.byte, d.bit );
-                for ( int bx = 0; bx < d.byte; ++bx )
-                {
-                    printf ( "%3d(0x%02x) ", d.buf[bx], d.buf[bx] );
-                }
-
-                int csum = calc_checksum ( d.buf, d.byte );
-
-                if ( csum < 0 )
-                {
-                    printf ( "\nChecksum BAD, bad length\n" );
-                }
-                else if ( csum == d.buf[10] )
-                {
-                    printf ( "\nChecksum OK\n" );
-                }
-                else
-                {
-                    // TODO checksum doesn't work
-                    printf ( "\n(FIXME)Checksum BAD, (expected) %d != (buf) %d\n", csum, d.buf[10] );
-                }
-
-                // TODO decode kWh
-
-                memset ( &d, 0, sizeof ( d ) );
-            }
-
             d.n_down = 0;
         }
         else if ( s < 0 && d.n_up > 0 )
         {
             // Negative edge
-            
+
             d.last_n_up = d.n_up;
             d.n_up = 0;
-        }
-        else if ( s < 0 )
-        {
-            // Negative level
             
-            // TODO look for 100+ samples down preceeding 40+ samples up for preamble
+            ++d.n_down;
             
-            if ( d.mode == 0 && d.last_n_up > 40 )
+            if ( d.mode == NegPreamble )
             {
-                // Preamble found
-                d.mode = 1;
-                d.last_n_up = 0;
+                if ( d.last_n_up > 40 )
+                {
+                    // Preamble found
+                    d.mode = Decode;
+                    d.last_n_up = 0;
+                }
+                else
+                {
+                    // False alarm, just garbage, clear everything
+                    memset ( &d, 0, sizeof ( d ) );
+                }
             }
-            else if ( d.mode == 1 && d.n_down == 0 )
+            else if ( d.mode == Decode )
             {
-                // TODO is this the correct 1/0 mapping?
+                // MSB
                 if ( d.last_n_up > 3 && d.last_n_up < 10 )
                 {
                     ++d.bit;
@@ -140,7 +153,7 @@ int main( int argc, char *argv[] )
                 }
                 else if ( d.last_n_up > 10 )
                 {
-                    d.buf[d.byte] |= ( 1 << d.bit );
+                    d.buf[d.byte] |= ( 0x80U >> d.bit );
                     ++d.bit;
                     printf ( "1" );
                 }
@@ -151,9 +164,9 @@ int main( int argc, char *argv[] )
 
                     d.bit = 0;
 
-                    if ( d.byte > 19 )
+                    if ( d.byte > 12 )
                     {
-                        printf ( "\nError, unknown format (byte>19), ignoring.\n\n" );
+                        printf ( "\nError, unknown format (byte>12), ignoring.\n\n" );
 
                         memset ( &d, 0, sizeof ( d ) );
                     }
@@ -163,8 +176,44 @@ int main( int argc, char *argv[] )
                     }
                 }
             }
+        }
+        else if ( s < 0 )
+        {
+            // Negative level
 
             ++d.n_down;
+
+            if ( d.mode == None && d.n_down > 50 )
+            {
+                d.mode = NegPreamble;
+            }
+            // 40+ samples down while in decode mode followed by up = end of message
+            else if ( d.mode == Decode && d.n_down > 40 )
+            {
+                if ( d.byte == 12 && d.bit == 0 )
+                {
+                    for ( int bx = 0; bx < d.byte; ++bx )
+                    {
+                        printf ( "%3d(0x%02x) ", d.buf[bx], d.buf[bx] );
+                    }
+
+                    const uint16_t rcv_csum = d.buf[10] << 8 | d.buf[11];
+                    const uint16_t csum = crc16xmodem ( d.buf, 10);
+
+                    if ( csum == rcv_csum )
+                    {
+                        printf ( "\nCRC OK\n" );
+
+                        // TODO decode kWh
+                    }
+                    else
+                    {
+                        printf ( "\nCRC BAD, (expected) %04x != (buf) %04x\n", csum, rcv_csum );
+                    }
+                }
+
+                memset ( &d, 0, sizeof ( d ) );
+            }
         }
     }
 
